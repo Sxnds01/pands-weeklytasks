7d2719fcb7138b5bca6acd0c17cac50fe718158e
Sandra Donatus <sndrdonatus@gmail.com>
2024-02-20 17:42:15 +0000

    weekly task two completed with bank.py

---
+++ week02/bank.py
@@ -0,0 +1,9 @@
+# Bank.py
+# prompts the user to read in two money amounts and add them
+# Author: Sandra Donatus
+
+amount1 = int(input('Enter amount1 (in cent):'))
+amount2 = int(input ('Enter amount2 (in cent):'))
+answer = float(amount1 + amount2)
+answer/100
+print(f"The sum of these is: €{answer/100:0.2f}")
