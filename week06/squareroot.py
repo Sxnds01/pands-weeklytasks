ae009a11252e21223cd8fac15dc7637f18a2478a
Sandra Donatus <sndrdonatus@gmail.com>
2024-04-07 20:11:39 +0100

    square root task first try

---
+++ week06/squareroot.py
@@ -0,0 +1,11 @@
+#print(9**(1/2))
+
+
+#number = input('Please enter a positive number:') - always get errors when using this function
+number = 100
+def squareroot(number):
+    print(number)
+    return(number**(1/2)) #the square root of a number is equal to the power of a half - 1/2
+
+ans = squareroot(100)
+print(f'The square root of {number} is approx. {ans}')
